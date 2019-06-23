ed05a921a246c2e82e5954690593a5465c7dfeb4
Artemii Petrovskii <artemii.petrovskii@outlook.com>
2019-06-23 18:32:27 +0300

    modified

--- PingPong/engine/PingPongEngine.java
+++ PingPong/engine/PingPongEngine.java
@@ -1,34 +1,8 @@
-package engine;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import screens.*;
+package screens;
+import engine.PingPongEngine;
 
-public class PingPongGameEngine implements
- MouseListener, MouseMotionListener, GameConstants{
-    PingPongGreenTable table;
-    public int kidRacket_Y = KID_RACKET_Y_START;
-    public PingPongGameEngine(PingPongGreenTable greenTable){
-         table = greenTable;
-}
-    public void mousePressed(MouseEvent e) {
-    table.point.x = e.getX();
-    table.point.y = e.getY();
-    table.repaint();
-}
-    public void mouseReleased(MouseEvent e) {};
-    public void mouseEntered(MouseEvent e) {};
-    public void mouseExited(MouseEvent e) {}; 
-    public void mouseClicked(MouseEvent e) {};
-    public void mouseDragged(MouseEvent e) {}
-    public void mouseMoved(MouseEvent e) {
-    int mouse_Y = e.getY();
-    if (mouse_Y < kidRacket_Y && kidRacket_Y > TABLE_TOP){
-       kidRacket_Y -= RACKET_INCREMENT;
-  } else if (kidRacket_Y < TABLE_BOTTOM) {
-       kidRacket_Y += RACKET_INCREMENT;
+public class PingPongTable {
+   public static void main(String[] args) {
+   PingPongEngine gameEngine = new PingPongEngine();
   }
-   table.setKidRacket_Y(kidRacket_Y);
-!!!!!!!!table.repaint();
- }
-}
+}
